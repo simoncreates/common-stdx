d2e00f927f33d845886a3b3d6214ebe591988612
Smon <smon@debian.somethin.com>
2025-08-05 20:40:09 +0200

    fixed rects

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-mod point;
 
+mod point;
 pub use point::Point;
 
-
-#[derive(Debug)]
-pub struct Rect {
-    pub p1: Point,
-    pub p2: Point,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Rect<T> {
+    pub p1: Point<T>,
+    pub p2: Point<T>,
 }
+
+impl<T> Rect<T> {
+    pub fn new(p1: Point<T>, p2: Point<T>) -> Self {
+        Rect { p1, p2 }
+    }
+}
+
